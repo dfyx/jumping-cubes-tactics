c8fece33fccb7cfb9b6ce62807a1a31e1702bda8
DFYX <dfyx@wecallit42.de>
2013-11-20 11:43:11 +0100

    Initialize project

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics.hpp>
+
+
+////////////////////////////////////////////////////////////
+/// Entry point of application
+///
+/// \return Application exit code
+///
+////////////////////////////////////////////////////////////
+int main()
+{
+    // Request a 32-bits depth buffer when creating the window
+    sf::ContextSettings contextSettings;
+    contextSettings.depthBits = 32;
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "JCT-SFML", sf::Style::Default, contextSettings);
+    window.setVerticalSyncEnabled(true);
+
+    // Start game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            // Escape key : exit
+            if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
+                window.close();
+        }
+
+        // Finally, display the rendered frame on screen
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
