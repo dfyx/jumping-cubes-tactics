3cd10cdcb63730c07158acf7f8bc4a6291ad4752
DFYX <dfyx@wecallit42.de>
2013-12-30 22:26:13 +0100

    Add main loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,18 @@
 ////////////////////////////////////////////////////////////
 #include <SFML/Graphics.hpp>
 
+#include "gamedata/game.h"
+#include "gamedata/player.h"
+#include "gamelogic/turncontroller.h"
+#include "graphics/node_sprite.h"
+#include "graphics/edge_sprite.h"
+
+#include "loaders/simple_game_loader.h"
+
+#include <iostream>
+
+using namespace JCT_SFML;
+
 
 ////////////////////////////////////////////////////////////
 /// Entry point of application
@@ -19,8 +31,23 @@ int main()
     contextSettings.antialiasingLevel = 8;
 
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "JCT-SFML", sf::Style::Default, contextSettings);
+    sf::RenderWindow window(sf::VideoMode(1280, 800), "JCT-SFML", sf::Style::Default, contextSettings);
     window.setVerticalSyncEnabled(true);
+
+    Loaders::SimpleGameLoader loader("data/risk.jct");
+
+    std::vector<Graphics::NodeSprite*> nodeSprites = loader.nodeSprites();
+    std::vector<Graphics::EdgeSprite*> edgeSprites = loader.edgeSprites();
+
+    Gamedata::Game *game = loader.game();
+    game->addPlayer("Player 1", 0xFF, 0x00, 0x00);
+    game->addPlayer("Player 2", 0x66, 0x66, 0xFF);
+    game->addPlayer("Player 3", 0x00, 0xFF, 0x00);
+    game->addPlayer("Player 4", 0xFF, 0x00, 0xFF);
+
+    Gamelogic::Turncontroller controller(game);
+
+    Graphics::NodeSprite *highlightedNode = NULL;
 
     // Start game loop
     while (window.isOpen())
@@ -38,9 +65,56 @@ int main()
                 window.close();
         }
 
+        // Handle mouse input
+        {
+            if(highlightedNode != NULL)
+            {
+                highlightedNode->setHighlighted(false);
+                highlightedNode = NULL;
+            }
+
+            sf::Vector2i intMousePos = sf::Mouse::getPosition(window);
+            sf::Vector2f mousePos(intMousePos.x, intMousePos.y);
+            std::vector<Graphics::NodeSprite*>::iterator nodeIter;
+            for(nodeIter = nodeSprites.begin(); nodeIter != nodeSprites.end(); nodeIter++)
+            {
+                Graphics::NodeSprite *nodeSprite = (*nodeIter);
+                if(nodeSprite->collides(mousePos) && controller.canIncreaseNode(nodeSprite->node()))
+                {
+                    highlightedNode = nodeSprite;
+                    nodeSprite->setHighlighted(true);
+
+                    if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && controller.increaseNode(nodeSprite->node()))
+                    {
+                        controller.startNextTurn();
+                    }
+                    break;
+                }
+            }
+        }
+
+        window.clear();
+
+        // Draw edges and nodes
+        {
+            std::vector<Graphics::EdgeSprite*>::iterator edgeIter;
+            for(edgeIter = edgeSprites.begin(); edgeIter != edgeSprites.end(); edgeIter++)
+            {
+                window.draw(**edgeIter);
+            }
+
+            std::vector<Graphics::NodeSprite*>::iterator nodeIter;
+            for(nodeIter = nodeSprites.begin(); nodeIter != nodeSprites.end(); nodeIter++)
+            {
+                window.draw(**nodeIter);
+            }
+        }
+
         // Finally, display the rendered frame on screen
         window.display();
     }
+
+    delete game;
 
     return EXIT_SUCCESS;
 }
